b0f47bfea4aed7cc24288bbe69acc37e5cec9d15
Basti Hoffmann <bo.hoffi@gmail.com>
2022-07-06 17:49:51 +0200

    chore: clear workspace

--- libs/ngx-localstorage/src/lib/tokens/storage-config.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { NgxLocalstorageConfiguration } from '../interfaces/storage-configuration';
-
-/**
- * Provides an injection token for the service configuration.
- */
-export const NGX_LOCAL_STORAGE_CONFIG = new InjectionToken<NgxLocalstorageConfiguration>('NgxLocalstorageConfiguration');
