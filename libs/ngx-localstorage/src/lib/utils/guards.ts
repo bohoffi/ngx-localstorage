f3ca8e9a2c4caa36e4eecfd307ac637fea78e194
bohoffi <bo.hoffi@gmail.com>
2022-07-21 22:23:39 +0200

    refactor: lift guard param type restrictions

--- libs/ngx-localstorage/src/lib/utils/guards.ts
+++ libs/ngx-localstorage/src/lib/utils/guards.ts
@@ -3,6 +3,6 @@ import { StorageSerializer } from '../interfaces/storage-serializer';
 /**
  * StorageSerializer Guard
  */
-export const isSerializer = (prefixOrSerializer?: string | StorageSerializer): prefixOrSerializer is StorageSerializer => {
+export const isSerializer = (prefixOrSerializer?: unknown): prefixOrSerializer is StorageSerializer => {
   return !!prefixOrSerializer && (prefixOrSerializer as StorageSerializer).serialize !== undefined;
 }
