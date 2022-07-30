18b0e3df0f55eb6b9a4739f757d80f4a34ad9b66
bohoffi <bo.hoffi@gmail.com>
2022-07-30 11:30:06 +0200

    doc: update demo

--- apps/ngx-localstorage-demo/src/app/modules/demo/demo.module.ts
+++ apps/ngx-localstorage-demo/src/app/modules/demo/demo.module.ts
@@ -6,7 +6,6 @@ import { MatCardModule } from '@angular/material/card';
 import { NgxLocalStorageModule } from 'ngx-localstorage';
 
 import { SubUsageComponent } from './sub-usage/sub-usage.component';
-import { DecoratorComponent } from './decorator/decorator.component';
 import { DirectiveComponent } from './directive/directive.component';
 import { StorageServiceComponent } from './storage-service/storage-service.component';
 import { EventStreamComponent } from './event-stream/event-stream.component';
@@ -23,8 +22,8 @@ import { MarkdownModule } from 'ngx-markdown';
     NgxLocalStorageModule.forRoot({ delimiter: '@', prefix: 'test' }),
     MarkdownModule.forChild()
   ],
-  declarations: [SubUsageComponent, DecoratorComponent, DirectiveComponent, StorageServiceComponent, EventStreamComponent],
-  exports: [SubUsageComponent, DecoratorComponent, DirectiveComponent, StorageServiceComponent, EventStreamComponent]
+  declarations: [SubUsageComponent, DirectiveComponent, StorageServiceComponent, EventStreamComponent],
+  exports: [SubUsageComponent, DirectiveComponent, StorageServiceComponent, EventStreamComponent]
 })
 export class DemoModule {
 }
