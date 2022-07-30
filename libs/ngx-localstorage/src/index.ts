2681110ad65e36ff9f83899b2817f5d0e4eea8d5
bohoffi <bo.hoffi@gmail.com>
2022-07-30 11:29:59 +0200

    refactor!: drop decorator support

--- libs/ngx-localstorage/src/index.ts
+++ libs/ngx-localstorage/src/index.ts
@@ -7,10 +7,7 @@ export * from './lib/ngx-localstorage.module';
 export * from './lib/services/ngx-localstorage.service';
 // directives
 export * from './lib/directives/ngx-localstorage.directive';
-// decorator
-export * from './lib/decorators/ngx-localstorage.decorator';
 // interfaces
-export * from './lib/interfaces/decorator-options';
 export * from './lib/interfaces/storage-configuration';
 export * from './lib/interfaces/storage-serializer';
 // tokens
