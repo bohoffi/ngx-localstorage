583216fb399b0e6e1dde81fa1fe6bbff7c1eac42
bohoffi <bo.hoffi@gmail.com>
2024-03-10 13:36:47 +0100

    chore: update scripts; remove tests

--- libs/ngx-localstorage/src/lib/directives/control-value-accessor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ControlValueAccessorDirective } from './control-value-accessor.directive';
-
-describe('ControlValueAccessorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ControlValueAccessorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- libs/ngx-localstorage/src/lib/directives/native-value-accessor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NativeValueAccessorDirective } from './native-value-accessor.directive';
-
-describe('NativeValueAccessorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NativeValueAccessorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
